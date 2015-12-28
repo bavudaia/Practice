9d60991862f04e86f16b12e10b32cce33dbd8ccb
bavudaia <bavudaia@BAVUDAIA-LAP.oradev.oraclecorp.com>
2015-12-28 23:56:50 +0530

    balance

---
+++ src/stack/BalancedParanthesis.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package stack;
+
+import utils.OperationUtils;
+
+/**
+ *
+ * @author bavudaia
+ */
+public class BalancedParanthesis {
+    public static void main(String[] args) {
+        //test case 1
+       String t1 = "[()]{}{[()()]()}";
+        System.out.println(check(t1));
+        //test case 2
+        String t2 = "[()]}{}{][()()]()}}";
+        System.out.println(check(t2));
+        String t3 = "[()]{}{][()()]()}}";
+        System.out.println(check(t3));
+        String t4 ="";
+        System.out.println(check(t4));
+    }
+    public static boolean check(String S)
+    {
+        Stack<Character> stack = new Stack<>();
+        int len = S.length();
+        for(int i=0;i<len;i++)
+        {
+            char ch = S.charAt(i);
+            if(OperationUtils.isOpenBracket(ch))
+            {
+                stack.push(ch);
+            }
+            else if(OperationUtils.isCloseBracket(ch))
+            {
+                char openExpected = OperationUtils.getOpenBracket(ch);
+                if(stack.isEmpty())
+                    return false;
+                else
+                {
+                    char openActual = stack.pop();
+                    if(openActual != openExpected)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        if(stack.isEmpty())
+            return true;
+        else
+            return false;
+    }
+}
